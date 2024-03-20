837fc6bc2b28da0e62763a7fc8c8c04b93319418
Veer-Parikh <parikhveer71@gmail.com>
2024-03-20 13:34:36 +0530

    fix:minor updates

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ fastify.get('/', async (req, res) => {
   return { hello: 'world' };
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 try {
   fastify.listen({port});
