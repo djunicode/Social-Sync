d66d34c5d18761a7962a4f737beb876e0663feb6
Sharans23 <sharan.shetty2309@gmail.com>
2024-04-12 19:30:25 +0530

    Payment 2

---
+++ src/app/payment.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MakePaymentComponent from "../components/MakePaymentComponent";
+
+const payment = () => {
+  return (
+    <div>
+      <h4>Payment page</h4>
+      <MakePaymentComponent />
+    </div>
+  );
+};
+
+export default payment;
