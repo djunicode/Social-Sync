7acb104ad7e917e0eb33eb1d546fc151e4dccfc6
Sahil Chaudhari <sahilchaudhari2004@gmail.com>
2024-04-30 19:50:28 +0530

    pull req failed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -23,7 +23,7 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:to-blue-700 before:dark:opacity-10 after:from-sky-900 after:via-[#0141ff] after:opacity-40 before:lg:h-[360px] z-[-1]">
-       <h1 className=" md:text-9xl text-4xl text-center font-extrabold">Social-Sync</h1>
+        <h1 className=" md:text-9xl text-4xl text-center font-extrabold">Social-Sync</h1>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -55,7 +55,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Log in page with integrated backend
+            Log in page with integrated backend
           </p>
         </Link>
 
