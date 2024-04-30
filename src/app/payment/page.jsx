404d39fb0d2ab03620b4c8bd196b85e62ff1fa15
Sharans23 <sharan.shetty2309@gmail.com>
2024-04-30 21:47:08 +0530

    Payment Stripe

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,14 +1,21 @@
-"use-client"
+"use-client";
 import React from "react";
-import razorpay from "../../app/razorpay";
 import dynamic from "next/dynamic";
-import MakePaymentComponent from "../../components/MakePaymentComponent";
+
 
 const payment = () => {
   return (
-    <div>
-      <h4>Payment page</h4>
-      <MakePaymentComponent />
+    <div className="m-10">
+      <h4 className="">Payment page</h4>
+      <button className="bg-gray">
+        <a
+          href="https://buy.stripe.com/test_7sIcQs6cFb1f33acMM"
+          // className="bg-white text-black"
+        >
+          Chat Pay: 100Rs
+        </a>
+      </button>
+      {/* <MakePaymentComponent /> */}
     </div>
   );
 };
--- src/app/videos/page.jsx
+++ src/app/videos/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+  "use client";
 import React from "react";
 import SearchBar from "@/components/SearchBar";
 import Sidebar from "@/components/Sidebar";
